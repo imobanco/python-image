74813a203efcf5d89488dbce36969dcb3da8e457
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2023-01-04 16:26:24 -0300

    Atualiza podman e faz direnv funcinar no nix develop (#12)
    * Adiciona podman-rootless como input e cria .envrc
    
    * Adiciona .direnv/ no .gitignore
    
    * Remove arquivos do .direnv/ versionados errôneamente
    
    * Injeta podman-rootless e remove podmanSetupScript (jeito antigo de criar os arquivos de config)

--- flake.nix
+++ flake.nix
@@ -3,9 +3,10 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    podman-rootless.url = "github:ES-Nix/podman-rootless/from-nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, podman-rootless }:
     flake-utils.lib.eachDefaultSystem (system:
       let
 
@@ -13,42 +14,18 @@
           system = "x86_64-linux";
           config = { allowUnfree = true; };
         };
-
-        # Provides a script that copies required files to ~/
-        podmanSetupScript =
-          let
-            registriesConf = pkgsAllowUnfree.writeText "registries.conf" ''
-              [registries.search]
-              registries = ['docker.io']
-              [registries.block]
-              registries = []
-            '';
-          in
-          pkgsAllowUnfree.writeShellScriptBin "podman-setup-script" ''
-            # Dont overwrite customised configuration
-            if ! test -f ~/.config/containers/policy.json; then
-              install -Dm555 ${pkgsAllowUnfree.skopeo.src}/default-policy.json ~/.config/containers/policy.json
-            fi
-
-            if ! test -f ~/.config/containers/registries.conf; then
-              install -Dm555 ${registriesConf} ~/.config/containers/registries.conf
-            fi
-          '';
-
       in
       {
 
         devShell = pkgsAllowUnfree.mkShell {
           buildInputs = with pkgsAllowUnfree; [
+            bashInteractive
             gnumake
-            podman
-            podmanSetupScript
+            podman-rootless.packages.${system}.podman
           ];
 
           shellHook = ''
             export TMPDIR=/tmp
-
-            podman-setup-script
 
             echo "Entering the nix devShell"
           '';
