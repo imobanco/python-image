7e6be20d451047518d936c71f92c03c965d7fef2
PedroRegisPOAR <pedroalencarregis@hotmail.com>
2023-09-01 13:48:29 -0300

    Adiciona suporte a aarch64-linux (#15)
    * Inicia suport a aarch64-linux
    
    * Usa inherit system;
    
    * Add coreutils
    
    * Refatora inputs
    
    * Refatora outputs e cached devShell
    
    * Refatora outputs e cached devShell, fix
    
    * Fix echo
    
    * Fix devShells.default
    
    * Testa instalação igual a do org
    
    * Testa instalação igual a do org, 2

--- flake.nix
+++ flake.nix
@@ -2,30 +2,54 @@
   description = "This is a nix with flakes package";
 
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+
     flake-utils.url = "github:numtide/flake-utils";
     podman-rootless.url = "github:ES-Nix/podman-rootless/from-nixpkgs";
+
+    podman-rootless.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils, podman-rootless }:
+  outputs = attrs@{
+    self,
+    nixpkgs,
+    flake-utils,
+    podman-rootless
+  }:
     flake-utils.lib.eachDefaultSystem (system:
       let
 
         pkgsAllowUnfree = import nixpkgs {
-          system = "x86_64-linux";
+          inherit system;
           config = { allowUnfree = true; };
         };
+
+        # https://gist.github.com/tpwrules/34db43e0e2e9d0b72d30534ad2cda66d#file-flake-nix-L28
+        pleaseKeepMyInputs = pkgsAllowUnfree.writeTextDir "bin/.please-keep-my-inputs"
+          (builtins.concatStringsSep " " (builtins.attrValues attrs));
       in
       {
 
-        devShell = pkgsAllowUnfree.mkShell {
+        devShells.default = pkgsAllowUnfree.mkShell {
           buildInputs = with pkgsAllowUnfree; [
             bashInteractive
+            coreutils
             gnumake
             podman-rootless.packages.${system}.podman
+            pleaseKeepMyInputs
           ];
 
           shellHook = ''
             export TMPDIR=/tmp
+
+            test -d .profiles || mkdir -v .profiles
+
+            test -L .profiles/dev \
+            || nix develop .# --profile .profiles/dev --command echo
+
+            test -L .profiles/dev-shell-default \
+            || nix build $(nix eval --impure --raw .#devShells."$system".default.drvPath) --out-link .profiles/dev-shell-"$system"-default
 
             echo "Entering the nix devShell"
           '';
